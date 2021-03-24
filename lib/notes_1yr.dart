8c31ee28f174bf8f7b760f821de60d30a298c122
rabhi <gitabhi26@gmail.com>
2021-03-25 00:18:09 +0530

    coursenotes

--- lib/notes_1yr.dart
+++ lib/notes_1yr.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'dart:async';
 
 class detail {
   final String name, asset, subtext;
@@ -36,111 +34,87 @@ class yearone extends StatelessWidget {
   }
 
   ListView _sem1() {
-    FutureBuilder(
-        future: json1(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (!snapshot.hasData) return Container();
-          return ListView.builder(
-              itemCount: snapshot.data.length(),
-              itemBuilder: (BuildContext context, int index) => Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Container(
-                      child: FittedBox(
-                        child: Material(
-                            color: Colors.white,
-                            elevation: 10.0,
-                            borderRadius: BorderRadius.circular(24.0),
-                            shadowColor: Color(0x802196F3),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Container(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 16.0),
-                                    child: tableft(snapshot.data[index].name,
-                                        snapshot.data[index].subtext),
-                                  ),
+    return ListView.builder(
+        itemCount: 3,
+        itemBuilder: (BuildContext context, int index) => Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                child: FittedBox(
+                  child: Material(
+                      color: Colors.white,
+                      elevation: 10.0,
+                      borderRadius: BorderRadius.circular(24.0),
+                      shadowColor: Color(0x802196F3),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 16.0),
+                              child: tableft("subject", "subtext"),
+                            ),
+                          ),
+                          Container(
+                            width: 190,
+                            height: 100,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(24.0),
+                              child: Image(
+                                fit: BoxFit.contain,
+                                alignment: Alignment.topRight,
+                                image: NetworkImage(
+                                  "https://images.unsplash.com/photo-1495147466023-ac5c588e2e94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
                                 ),
-                                Container(
-                                  width: 190,
-                                  height: 100,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(24.0),
-                                    child: Image(
-                                      fit: BoxFit.contain,
-                                      alignment: Alignment.topRight,
-                                      image: NetworkImage(
-                                        snapshot.data[index].asset,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )),
-                      ),
-                    ),
-                  ));
-        });
-  }
-
-  ListView _sem2() {
-    FutureBuilder(
-        future: json2(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (!snapshot.hasData) return Container();
-          return ListView.builder(
-              itemCount: snapshot.data.length(),
-              itemBuilder: (BuildContext context, int index) => Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Container(
-                      child: FittedBox(
-                        child: Material(
-                            color: Colors.white,
-                            elevation: 10.0,
-                            borderRadius: BorderRadius.circular(24.0),
-                            shadowColor: Color(0x802196F3),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Container(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 16.0),
-                                    child: tabright(snapshot.data[index].name,
-                                        snapshot.data[index].subtext),
-                                  ),
-                                ),
-                                Container(
-                                  width: 190,
-                                  height: 100,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(24.0),
-                                    child: Image(
-                                      fit: BoxFit.contain,
-                                      alignment: Alignment.topRight,
-                                      image: NetworkImage(
-                                          snapshot.data[index].asset),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )),
-                      ),
-                    ),
-                  ));
-        });
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
+              ),
+            ));
   }
 }
 
-Future<List<detail>> json1({BuildContext context}) async {
-  var list =
-      await DefaultAssetBundle.of(context).loadString('assets/json/sem1.json');
-  return json.decode(list);
-}
-
-Future<List<detail>> json2({BuildContext context}) async {
-  var list =
-      await DefaultAssetBundle.of(context).loadString('assets/json/sem2.json');
-  return json.decode(list);
+ListView _sem2() {
+  return ListView.builder(
+      itemCount: 3,
+      itemBuilder: (BuildContext context, int index) => Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              child: FittedBox(
+                child: Material(
+                    color: Colors.white,
+                    elevation: 10.0,
+                    borderRadius: BorderRadius.circular(24.0),
+                    shadowColor: Color(0x802196F3),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 16.0),
+                            child: tabright("subject", "subtext"),
+                          ),
+                        ),
+                        Container(
+                          width: 190,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(24.0),
+                            child: Image(
+                              fit: BoxFit.contain,
+                              alignment: Alignment.topRight,
+                              image: NetworkImage(
+                                  "https://images.unsplash.com/photo-1495147466023-ac5c588e2e94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            ),
+          ));
 }
 
 Widget tableft(String s, String subtext) {
