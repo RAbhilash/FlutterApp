0c2c017c247ed8c06d08d2102fb2e5719839a6f4
rabhi <gitabhi26@gmail.com>
2021-04-29 22:23:18 +0530

    Co-authored-by: AkashJOY007 <AkashJOY007@users.noreply.github.com>

--- lib/placment.dart
+++ lib/placment.dart
@@ -1,9 +1,11 @@
+import 'package:app/fontlib/my_flutter_app_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class detail {
-  String subject, asset, url;
-  detail(this.subject, this.asset, this.url);
+  String subject, url, subtext;
+  IconData asset;
+  detail(this.subject, this.asset, this.url, this.subtext);
 }
 
 launchurl(url) async {
@@ -18,28 +20,51 @@ class placement extends StatelessWidget {
   @override
   List<detail> data1 = [];
   placement() {
-    data1.add(detail("DBMS", "assets/images/thumbnail.jpg",
-        "https://whimsical.com/dbms-roadmap-by-love-babbar-FmUi8ffVop33t3MmpVxPCo"));
-    data1.add(detail("Final year roadmap", "assets/images/thumbnail.jpg",
-        "https://www.youtube.com/watch?v=VY6003vijLw"));
-    data1.add(detail("Web Development", "assets/images/thumbnail.jpg",
-        "https://www.youtube.com/watch?v=GLk7-imcjiI"));
-    data1.add(detail("OOPS", "assets/images/thumbnail.jpg",
-        "https://whimsical.com/object-oriented-programming-cheatsheet-by-love-babbar-YbSgLatbWQ4R5paV7EgqFw"));
-    data1.add(detail("2nd year", "assets/images/thumbnail.jpg",
-        "https://www.youtube.com/watch?v=CF1j_wMEbPA"));
+    data1.add(detail(
+        "DBMS",
+        MyFlutterApp.whimsical,
+        "https://whimsical.com/dbms-roadmap-by-love-babbar-FmUi8ffVop33t3MmpVxPCo",
+        "DBMS RoadMap - Love Babbar"));
+    data1.add(detail(
+        "Final year roadmap",
+        MyFlutterApp.youtube,
+        "https://www.youtube.com/watch?v=VY6003vijLw",
+        "[4th year] Roadmap to Dream Placement - Love Babbar"));
+    data1.add(detail(
+        "Web Development",
+        MyFlutterApp.youtube,
+        "https://www.youtube.com/watch?v=GLk7-imcjiI",
+        "Complete Roadmap for Web Development - Love Babbar"));
+    data1.add(detail(
+        "OOPS",
+        MyFlutterApp.whimsical,
+        "https://whimsical.com/object-oriented-programming-cheatsheet-by-love-babbar-YbSgLatbWQ4R5paV7EgqFw",
+        "Object Oriented Programming CheatSheet - by Love Babbar"));
+    data1.add(detail(
+        "2nd year",
+        MyFlutterApp.youtube,
+        "https://www.youtube.com/watch?v=CF1j_wMEbPA",
+        "Roadmap to Off-campus Internship in 2nd Year - Apna College"));
     data1.add(detail(
         "How to get into Product Based Companies",
-        "assets/images/thumbnail.jpg",
-        "https://www.youtube.com/watch?v=PihtA9D-IlE"));
+        MyFlutterApp.youtube,
+        "https://www.youtube.com/watch?v=PihtA9D-IlE",
+        "Studying for College Placements - Aman Dhatarwal"));
     Comparator<detail> comp = (a, b) => a.subject.compareTo(b.subject);
     data1.sort(comp);
   }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Third Year'),
+        backgroundColor: Colors.pink[50],
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        title: Text('Placement Resources',
+            style: TextStyle(
+              color: Colors.black,
+            )),
+        centerTitle: true,
       ),
       body: _sem(data1),
     );
@@ -47,60 +72,37 @@ class placement extends StatelessWidget {
 
   ListView _sem(List<detail> data) {
     return ListView.builder(
+        padding: EdgeInsets.all(16),
         itemCount: data.length,
-        itemBuilder: (BuildContext context, int index) => item(context, index,
-            data[index].subject, data[index].url, data[index].asset));
+        itemBuilder: (BuildContext context, int index) => item(
+            context,
+            index,
+            data[index].subject,
+            data[index].url,
+            data[index].asset,
+            data[index].subtext));
   }
 }
 
-Widget item(
-    BuildContext context, int index, String subject, String url, String asset) {
-  return Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: Container(
-      child: FittedBox(
-        child: Material(
-            color: Colors.white,
-            elevation: 10.0,
-            borderRadius: BorderRadius.circular(24.0),
-            shadowColor: Color(0x802196F3),
-            child: Container(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: tab(context, subject, url),
-              ),
-            )),
+// double size = 100.0;
+Widget item(BuildContext context, int index, String subject, String url,
+    IconData asset, String subtext) {
+  return Card(
+    margin: EdgeInsets.all(16),
+    elevation: 10,
+    child: InkWell(
+      onTap: () {
+        launchurl(url);
+      },
+      child: Container(
+        child: ListTile(
+            subtitle: Text(subtext),
+            contentPadding:
+                EdgeInsets.only(top: 8, right: 8, bottom: 8, left: 16),
+            trailing: Icon(Icons.arrow_forward),
+            leading: Icon(asset),
+            title: Text(subject)),
       ),
     ),
-  );
-}
-
-Widget tab(BuildContext context, String s, String url) {
-  var size = MediaQuery.of(context).size;
-  return Column(
-    children: <Widget>[
-      Padding(
-        padding: EdgeInsets.fromLTRB(5, 0, 0, 8),
-        child: Container(
-          width: size.width / 3.5,
-          child: Text(
-            s,
-            softWrap: true,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 1,
-          ),
-        ),
-      ),
-      Padding(
-        padding: EdgeInsets.only(left: 8),
-        child: ElevatedButton.icon(
-          label: Text('View'),
-          icon: Icon(Icons.download_rounded),
-          onPressed: () {
-            launchurl(url);
-          },
-        ),
-      ),
-    ],
   );
 }
